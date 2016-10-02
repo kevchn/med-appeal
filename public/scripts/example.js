b4c683c08370e26575686dbf03b71620cca7c854
Kevin Chen <kevchen98@gmail.com>
2016-10-01 22:31:35 -0400

    Cut author

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -20,9 +20,6 @@ var Comment = React.createClass({
   render: function() {
     return (
       <div className="comment">
-        <h2 className="commentAuthor">
-          {this.props.author}
-        </h2>
         <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
@@ -87,7 +84,7 @@ var CommentList = React.createClass({
   render: function() {
     var commentNodes = this.props.data.map(function(comment) {
       return (
-        <Comment author={comment.author} key={comment.id}>
+        <Comment>
           {comment.text}
         </Comment>
       );
@@ -102,33 +99,23 @@ var CommentList = React.createClass({
 
 var CommentForm = React.createClass({
   getInitialState: function() {
-    return {author: '', text: ''};
-  },
-  handleAuthorChange: function(e) {
-    this.setState({author: e.target.value});
+    return {text: ''};
   },
   handleTextChange: function(e) {
     this.setState({text: e.target.value});
   },
   handleSubmit: function(e) {
     e.preventDefault();
-    var author = this.state.author.trim();
     var text = this.state.text.trim();
-    if (!text || !author) {
+    if (!text) {
       return;
     }
-    this.props.onCommentSubmit({author: author, text: text});
-    this.setState({author: '', text: ''});
+    this.props.onCommentSubmit({text: text});
+    this.setState({text: ''});
   },
   render: function() {
     return (
       <form className="commentForm" onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          placeholder="Your name"
-          value={this.state.author}
-          onChange={this.handleAuthorChange}
-        />
         <input
           type="text"
           placeholder="Say something..."
